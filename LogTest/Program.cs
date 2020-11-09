32fcc0be7a90a70d8c045e76a22ebb1a4f049181
zjc <836025475@qq.com>
2020-11-09 17:28:31 +0800

    fix

--- LogTest/Program.cs
+++ LogTest/Program.cs
@@ -4,6 +4,7 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web.Script.Serialization;
@@ -16,15 +17,69 @@ namespace LogTest
         {
             //获取AccessToken
             string accessToken = GetAccessToken();
+            //获取审批痕迹
+            string flowRecord = GetFlowRecord(accessToken, "5fa8eebc4435e80001b80e41", "576db4870d6443ddb39d9026a5a426ac");
+            //获取审批状态
+            string flowStatus = GetFlowStatus(accessToken, );
             //获取外部接口日志
-            string externalLog =  GetExternalLog(accessToken);
-            //Console.WriteLine(externalLog);
+            //string externalLog =  GetExternalLog(accessToken);
+            //JObject json = JObject.Parse(externalLog);
+            //List<PushLogs> pushlogs = GetPushLogs(json["data"]["pushLogs"].ToString());
+            ////获取实例
+            //List<JObject> instance = new List<JObject>();
+            //pushlogs.ForEach(p =>
+            //{
+            //    string ins = GetInstance(p.FormInstId, p.FormCodeId, accessToken);
+            //    JObject jObject = JObject.Parse(ins);
+            //    string success = jObject["success"].ToString();
+            //    if (jObject["success"].ToString() == "True")
+            //        instance.Add(jObject);
+            //});
+            Console.WriteLine("Hello");
         }
-        //
+        /// <summary>
+        /// 查询审批状态
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="flowInstId"></param>
+        /// <returns></returns>
+        private static string GetFlowStatus(string accessToken, string flowInstId)
+        {
+            string url = "https://yunzhijia.com/gateway/workflow/form/thirdpart/getFlowRecord?accessToken=" + accessToken;
+            JObject param = new JObject();
+            param.Add("formInstId", flowInstId);
+            string response = PostUrl(url, param.ToString(), "application/json");
+            return response;
+        }
+        /// <summary>
+        /// 获取审批痕迹
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="formInstId"></param>
+        /// <param name="formCodeId"></param>
+        /// <returns></returns>
+        private static string GetFlowRecord(string accessToken, string formInstId, string formCodeId)
+        {
+            string url = "https://yunzhijia.com/gateway/workflow/form/thirdpart/getFlowRecord?accessToken=" + accessToken;
+            JObject param = new JObject();
+            param.Add("formInstId", formInstId);
+            param.Add("formCodeId", formCodeId);
+            string response = PostUrl(url, param.ToString(), "application/json");
+            return response;
+        }
+        //获取实例
+        public static string GetInstance(string formInstId, string formCodeId, string accessToken)
+        {
+            string url = "https://yunzhijia.com/gateway/workflow/form/thirdpart/viewFormInst?accessToken=" + accessToken;
+            JObject param = new JObject();
+            param.Add("formInstId", formInstId);
+            param.Add("formCodeId", formCodeId);
+            string response = PostUrl(url, param.ToString(), "application/json");
+            return response;
+        }
         private static string GetExternalLog(string accessToken)
         {
             string url = "https://yunzhijia.com/gateway/workflow/form/thirdpart/getPushLog?accessToken=" +  accessToken;
-            JavaScriptSerializer jss = new JavaScriptSerializer();
             //页码信息
             JObject pageable = new JObject();
             pageable.Add("id", null);
@@ -35,8 +90,8 @@ namespace LogTest
             postParam.Add("pageable", pageable);
             postParam.Add("devType", "user");
             postParam.Add("startTime", ConvertDateTimeToInt(DateTime.Now));
-            postParam.Add("endTime", ConvertDateTimeToInt(DateTime.Now));
-            postParam.Add("pushType", "success");
+            postParam.Add("endTime", ConvertDateTimeToInt(DateTime.Now.AddHours(1)));
+            postParam.Add("pushType", "failed");
             string jsonRequest = PostUrl(url, postParam.ToString(), "application/json");
             return jsonRequest.ToString();
         }
@@ -44,9 +99,9 @@ namespace LogTest
         private static string GetAccessToken()
         {
             JObject param = new JObject();
-            param.Add("appId", "xxxxxx");
+            param.Add("appId", "SP15452095");
             param.Add("eid", "15452095");
-            param.Add("secret", "xxxxxxxx");
+            param.Add("secret", "dxjGOSdfAtTwCEqmQTDKdAzKfau7bK");
             param.Add("timestamp", ConvertDateTimeToInt(DateTime.Now));
             param.Add("scope", "team");
             String url = "https://yunzhijia.com/gateway/oauth2/token/getAccessToken";
@@ -77,28 +132,18 @@ namespace LogTest
             try
             {
                 HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-
                 req.Method = "POST";
-
                 req.ContentType = contentType;// "application/x-www-form-urlencoded"
-
                 req.Timeout = 800;//请求超时时间
-
                 byte[] data = Encoding.UTF8.GetBytes(postData);
-
                 req.ContentLength = data.Length;
-
                 using (Stream reqStream = req.GetRequestStream())
                 {
                     reqStream.Write(data, 0, data.Length);
-
                     reqStream.Close();
                 }
-
                 HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-
                 Stream stream = resp.GetResponseStream();
-
                 //获取响应内容
                 using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
                 {
@@ -106,8 +151,14 @@ namespace LogTest
                 }
             }
             catch (Exception e) { }
-
             return result;
+        }
+        private static List<PushLogs> GetPushLogs(string jsonstr)
+        {
+            List<PushLogs> pushLogs = new List<PushLogs>();
+            JavaScriptSerializer Serializer = new JavaScriptSerializer();
+            pushLogs = Serializer.Deserialize<List<PushLogs>>(jsonstr);
+            return pushLogs;
         }
     }   
 }
---
+++ LogTest/PushLogs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogTest
+{
+    public class PushLogs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Eid { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FlowInstId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FormCodeId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FormDefId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FormInstId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string InterfaceId { get; set; }
+        /// <summary>
+        /// 接口1
+        /// </summary>
+        public string InterfaceName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Nodeid { get; set; }
+        /// <summary>
+        /// 起始节点
+        /// </summary>
+        public string NodeName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Success { get; set; }
+        /// <summary>
+        /// 模版标题
+        /// </summary>
+        public string TempName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public long Time { get; set; }
+    }
+}
